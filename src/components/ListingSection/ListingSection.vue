13002743e69d1a5d7e222a9a757e4bb2abeb9e9e
BigBigBigSweetBuns <dadadadoushabao@gmail.com>
2021-03-23 23:07:35 +0800

    add listingSection component

---
+++ src/components/ListingSection/ListingSection.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="listing-section"></div>
+</template>
+
+<script>
+export default {
+  name: "listingSection",
+  data: function () {
+    return {};
+  },
+};
+</script>
+
+<style scoped leng="scss">
+</style>
