5a1697424afb6315a5745c04297e6009c3670ced
BigBigBigSweetBuns <dadadadoushabao@gmail.com>
2021-03-23 20:44:45 +0800

    init

---
+++ src/components/ArticleMin.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="article">
+    <header>
+      <h1>henader</h1>
+    </header>
+    <div class="content" v-html="article.contents"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "articleMin",
+  data: function () {
+    return {
+      article: {
+        header: "header",
+        contents:
+          "<p>The TEDxSydney team has made it through another year and TEDxSydney 2020 online was a huge success.</p>",
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+.article {
+}
+header {
+  margin-top: 10px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -8,32 +8,31 @@ const routes = [
     path: '/',
     name: 'Home',
     component: () => import(/* webpackChunkName: "Home" */ '../views/Home/Home.vue')
+    // }, {
+    //   path: '/events',
+    //   name: 'Events',
+    //   component: () => import(/* webpackChunkName: "Events" */ '../views/Events/Events.vue')
+    // }, {
+    //   path: '/videos',
+    //   name: 'Videos',
+    //   component: () => import(/* webpackChunkName: "Videos" */ '../views/Videos/Videos.vue')
+    // }, {
+    //   path: '/speakers',
+    //   name: 'Speakers',
+    //   component: () => import(/* webpackChunkName: "Speakers" */ '../views/Speakers/Speakers.vue')
+    // }, {
+    //   path: '/all-news',
+    //   name: 'All-News',
+    //   component: () => import(/* webpackChunkName: "All-News" */ '../views/All-News/All-News.vue')
+    // }, {
+    //   path: '/parters',
+    //   name: 'Partners',
+    //   component: () => import(/* webpackChunkName: "Partners" */ '../views/Partners/Partners.vue')
+    // }, {
+    //   path: '/community',
+    //   name: 'Community',
+    //   component: () => import(/* webpackChunkName: "community" */ '../views/Community/Community.vue')
   }, {
-    path: '/',
-    name: 'Events',
-    component: () => import(/* webpackChunkName: "Evnets" */ '../views/Events/Events.vue')
-  }, {
-    path: '/videos',
-    name: 'Videos',
-    component: () => import(/* webpackChunkName: "Videos" */ '../views/Videos/Videos.vue')
-  }, {
-    path: '/speakers',
-    name: 'Speakers',
-    component: () => import(/* webpackChunkName: "Speakers" */ '../views/Speakers/Speakers.vue')
-  }, {
-    path: '/all-news',
-    name: 'All-News',
-    component: () => import(/* webpackChunkName: "All-News" */ '../views/All-News/All-News.vue')
-  }, {
-    path: '/parters',
-    name: 'Partners',
-    component: () => import(/* webpackChunkName: "Partners" */ '../views/Partners/Partners.vue')
-  }, {
-    path: '/community',
-    name: 'Community',
-    component: () => import(/* webpackChunkName: "community" */ '../views/Community/Community.vue')
-  },
-  {
     path: '/about',
     name: 'About',
     // route level code-splitting
