c20d4966cd2c8a91e28f43517e7a6be93064a64d
BigBigBigSweetBuns <dadadadoushabao@gmail.com>
2021-03-24 20:48:48 +0800

    add News views

---
+++ src/views/News/News.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="news">
+    <article-default></article-default>
+  </div>
+</template>
+
+<script>
+import ArticleDefault from "@/components/Article/ArticleDefault.vue";
+export default {
+  name: "news",
+  components: { ArticleDefault },
+  data: function () {
+    return {
+      article: {
+        title: "TEDxSydney 2020 Presents Discovery Sessions",
+        theme: "News",
+        contentHMTL: "content",
+      },
+    };
+  },
+  created: function () {
+    // 请求数据
+  },
+};
+</script>
+<style lang="sass" scoped>
+</style>
