31efd67fa5d62957f162033fb1d56f3622bca2c8
BigBigBigSweetBuns <dadadadoushabao@gmail.com>
2021-03-25 18:04:53 +0800

    update GridDivider components

--- src/components/GridDivider/GridDivider.vue
+++ src/components/GridDivider/GridDivider.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid-divider"></div>
+  <b-container class="grid-divider"></b-container>
 </template>
 
 <script>
@@ -7,9 +7,13 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
+@import "@/assets/variable";
+@mixin background($path){
+  background: url($imagesBaseURL+$path) repeat-x;
+}
 .grid-divider {
   height: 12px;
   margin: 35px auto;
-  background: url("./img/arrow-repeat.png") repeat-x;
+  @include background("arrow-repeat.png?Expires=1616670165&OSSAccessKeyId=TMP.3KfTEV8UvBdxvLn9s5bTbGxtGnn6uaoPqhV3v7hVBfGbtDBuBGp231p7PxobHAQnZccjnGD5Y6W8FLYzraY7dqEQav45Lm&Signature=ANcCwsE6JWAWsnh7NTwOMbxuIjQ%3D&versionId=CAEQHRiBgID72OKiwxciIGZjOGYzZmQ5MDY3OTRlZTdhMDU4MWRjYjIyYjgwNmY0&response-content-type=application%2Foctet-stream");
 }
 </style>
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <article-min />
+    <grid-divider></grid-divider>
     <listing-section></listing-section>
   </div>
 </template>
@@ -9,11 +10,13 @@
 // @ is an alias to /src
 import ArticleMin from "@/components/Article/ArticleMin.vue";
 import ListingSection from "@/components/ListingSection/ListingSection.vue";
+import GridDivider from '../../components/GridDivider/GridDivider.vue';
 export default {
   name: "Home",
   components: {
     ArticleMin,
     ListingSection,
+    GridDivider,
   },
   methods: {
     // onSlideStart(slide) {
