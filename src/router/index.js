432f5e05198e469082917a2719829e5b51114e55
BigBigBigSweetBuns <dadadadoushabao@gmail.com>
2021-03-28 01:19:19 +0800

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -9,18 +9,18 @@ const routes = [
     name: 'Home',
     component: () => import(/* webpackChunkName: "Home" */ '../views/Home/Home.vue')
   }, {
-      path: '/events',
-      name: 'Events',
-      component: () => import(/* webpackChunkName: "Events" */ '../views/Events/Events.vue')
-    }, {
-      path: '/videos',
-      name: 'Videos',
-      component: () => import(/* webpackChunkName: "Videos" */ '../views/Videos/Videos.vue')
-    }, {
-      path: '/speakers',
-      name: 'Speakers',
-      component: () => import(/* webpackChunkName: "Speakers" */ '../views/Speakers/Speakers.vue')
-    }, {
+    path: '/events',
+    name: 'Events',
+    component: () => import(/* webpackChunkName: "Events" */ '../views/Events/Events.vue')
+  }, {
+    path: '/videos',
+    name: 'Videos',
+    component: () => import(/* webpackChunkName: "Videos" */ '../views/Videos/Videos.vue')
+  }, {
+    path: '/speakers',
+    name: 'Speakers',
+    component: () => import(/* webpackChunkName: "Speakers" */ '../views/Speakers/Speakers.vue')
+  }, {
     path: '/all-news',
     name: 'All-News',
     component: () => import(/* webpackChunkName: "All-News" */ '../views/News/All-News.vue')
@@ -29,20 +29,21 @@ const routes = [
     name: 'News',
     component: () => import(/* webpackChunkName: "All-News" */ '../views/News/News.vue')
   }, {
-      path: '/parters',
-      name: 'Partners',
-      component: () => import(/* webpackChunkName: "Partners" */ '../views/Partners/Partners.vue')
-    }, {
-      path: '/community',
-      name: 'Community',
-      component: () => import(/* webpackChunkName: "community" */ '../views/Community/Community.vue')
+    path: '/parters',
+    name: 'Partners',
+    component: () => import(/* webpackChunkName: "Partners" */ '../views/Partners/Partners.vue')
+  }, {
+    path: '/community',
+    name: 'Community',
+    component: () => import(/* webpackChunkName: "community" */ '../views/Community/Community.vue')
   }, {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About/About.vue'),
+  }, {
+    path: '/about/:id',
+    name: 'About',
+    component: () => import(/* webpackChunkName: "All-News" */ '../views/News/News.vue'),
   }
 ]
 
