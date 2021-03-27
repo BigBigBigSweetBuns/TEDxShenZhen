a6a8e10b7763f73d7e71540304be02126f677341
BigBigBigSweetBuns <dadadadoushabao@gmail.com>
2021-03-27 20:33:47 +0800

    add cardsButton components

---
+++ src/components/Card/CardsButton.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="card-list">
+    <b-row>
+      <b-col
+        cols="12"
+        sm="6"
+        md="4"
+        v-for="(cardData, index) in cardsList"
+        :key="index"
+      >
+        <card-default
+          :title="cardData.title"
+          :introduction="cardData.introduction"
+          :thumbnail="cardData.thumbnail"
+          :href="cardData.href"
+        ></card-default>
+      </b-col>
+    </b-row>
+    <b-button class="mx-auto">LOAD MORE</b-button>
+  </div>
+</template>
+
+<script>
+import CardDefault from "@/components/Card/CardDefault.vue";
+export default {
+  name: "CardsButton",
+  components: { CardDefault },
+  props: {
+    cardsList: {
+      type: Array,
+      reuqired: true,
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/variable.scss";
+.card-list {
+  > .mx-auto {
+    display: block;
+  }
+}
+</style>
