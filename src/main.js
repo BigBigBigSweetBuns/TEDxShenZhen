68fc269272c3e8ef42351aa390257eb3f393a5ce
BigBigBigSweetBuns <dadadadoushabao@gmail.com>
2021-04-03 02:01:47 +0800

    add dependencies aioxs

--- src/main.js
+++ src/main.js
@@ -6,7 +6,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import "@/helpers/filers/filers"
+import axios from "axios"
 Vue.config.productionTip = false
+
+//配置axios
+// 本地
+axios.defaults.baseURL = 'http://localhost:3000/'
+// 
+// axios.defaults.baseURL = 'http://www.tedxshenzhen.cn/api/'
+Vue.prototype.$axios = axios
 
 import VueLazyLoad from "vue-lazyload"
 Vue.use(VueLazyLoad, {
