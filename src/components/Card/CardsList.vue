ea7b83f7a5d8208c6aa512be1f33feca75fa6c1e
BigBigBigSweetBuns <dadadadoushabao@gmail.com>
2021-04-03 23:46:04 +0800

    update all-news and add CardsList component

---
+++ src/components/Card/CardsList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card-list">
+    <b-row>
+      <b-col
+        cols="12"
+        sm="6"
+        md="4"
+        v-for="(cardData, index) in cardsList"
+        :key="index"
+      >
+        <card-default
+          :title="cardData.title"
+          :introduction="cardData.introduction"
+          :thumbnail="cardData.thumbnail"
+          :path="cardData.path"
+        ></card-default>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+import CardDefault from "@/components/Card/CardDefault.vue";
+export default {
+  name: "CardsList",
+  components: { CardDefault },
+  props: {
+    cardsList: {
+      type: Array,
+      reuqired: true,
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/variable.scss";
+.card-list {
+}
+</style>
--- src/views/News/All-News.vue
+++ src/views/News/All-News.vue
@@ -1,38 +1,87 @@
 <template>
   <div class="all-news">
+    <div class="spinner" v-if="loading">
+      <b-spinner label="Loading..."></b-spinner>
+    </div>
     <b-container>
-      <cards-button :cardsList="cardsList"></cards-button>
+      <cards-list :cardsList="cardsList"></cards-list>
+      <b-button
+        class="mx-auto"
+        v-if="!loading && !bottomout"
+        v-on:click="loadmore"
+        >LOAD MORE</b-button
+      >
     </b-container>
   </div>
 </template>
 
 <script>
-import CardsButton from "../../components/Card/CardsButton.vue";
+import CardsList from "../../components/Card/CardsList.vue";
 export default {
-  components: { CardsButton },
-  name: "All-News",
+  components: { CardsList },
+  name: "AllNews",
   data() {
     return {
       cardsList: [],
+      pageNum: 1,
+      loading: false,
+      bottomout: false,
     };
   },
+  methods: {
+    isbottomout(code) {
+      // 触底检测
+      if (code != 0) {
+        this.bottomout = true;
+      }
+    },
+    loadmore() {
+      this.loading = true;
+      this.pageNum++;
+      this.$axios
+        .get("/news/list", {
+          params: {
+            page: this.pageNum,
+          },
+        })
+        .then((res) => {
+          const result = res.data.result;
+          this.loading = false;
+          if (res.data.code == 0) {
+            this.cardsList.push(
+              ...result.map((e) => {
+                e.path = "/news" + e.path;
+                return e;
+              })
+            );
+          } else {
+            this.isbottomout(res.data.code);
+          }
+        });
+    },
+    getNewsList() {
+      this.$axios
+        .get("/news/list", {
+          params: {
+            page: this.pageNum,
+          },
+        })
+        .then((res) => {
+          const result = res.data.result;
+          if (res.data.code == 0) {
+            this.cardsList = result.map((e) => {
+              e.path = "/news" + e.path;
+              return e;
+            });
+          }
+        });
+    },
+  },
   created() {
-    this.$axios
-      .get("/news/list", {
-        params: {
-          page: 1,
-        },
-      })
-      .then((res) => {
-        const result = res.data.result;
-        console.log(result);
-        if (res.data.code == 0) {
-          this.cardsList = result.map((e) => {
-            e.path = "/news" + e.path;
-            return e;
-          });
-        }
-      });
+    this.getNewsList();
+  },
+  beforeRouteLeave() {
+    this.loading = false;
   },
 };
 </script>
@@ -41,5 +90,17 @@ export default {
 .all-news {
   margin-top: 2rem;
   margin-bottom: 2rem;
+  position: relative;
+  .spinner {
+    display: block;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 499;
+  }
+  .mx-auto {
+    display: block;
+  }
 }
 </style>
