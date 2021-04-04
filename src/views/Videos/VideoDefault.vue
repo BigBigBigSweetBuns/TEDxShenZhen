e821a8a0585ee5020a9f4692c0503277e2ddc8f8
BigBigBigSweetBuns <dadadadoushabao@gmail.com>
2021-04-04 16:10:47 +0800

    update videoDefault views

--- src/views/Videos/VideoDefault.vue
+++ src/views/Videos/VideoDefault.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="video-default">
     <b-container>
-      <iframe
-        ref="videoview"
-        :src="video.src"
-        scrolling="no"
-        border="0"
-        frameborder="no"
-        framespacing="0"
-        allowfullscreen="true"
-        :style="'height:' + iframeHeight"
-      >
-      </iframe>
-      <!-- <iframe :src="video.src"></iframe> -->
-      <h3>{{ video.title }}</h3>
-      <p v-html="video.introduction"></p>
+      <h3>{{ title }}</h3>
+      <div class="videoview" ref="videoview">
+        <iframe
+          :src="videoSrc"
+          scrolling="no"
+          border="0"
+          frameborder="no"
+          framespacing="0"
+          allowfullscreen="true"
+          width="100%"
+          :style="'height:' + iframeHeight"
+        >
+        </iframe>
+      </div>
+      <p v-html="contentHTML"></p>
     </b-container>
   </div>
 </template>
@@ -24,20 +25,34 @@ export default {
   name: "VideoDefault",
   data: function () {
     return {
-      video: {
-        title: "通过行走感受土地与自我的力量 | 半落@TEDxShenzhen",
-        introduction:
-          "<p>他用27 年时间，走过中国31个省市自治区，在大部分省待了超过一个月时间。自2011年至今，带领上万人以各种形式行走在中国大地上。他研究了美国国家步道对于美国特别是美国青年人的影响、朝圣之路对于欧洲的重要影响，以及西南联大的迁徙对于学生成长产生的深刻影响，希望陪伴青年人通过止语徒步中感受我们的国家。</p>" +
-          "<p>半落，曾为海军航空兵军官，在北海舰队、东海舰队服役。现为究径止语徒步创始人，在国内开辟了24条9-10天的止语徒步线路，结合重要历史文化节点，陪伴中国年轻人在行走中了解自己，感受中华。</p>",
-        src:
-          "//player.bilibili.com/player.html?aid=93272539&bvid=BV1hE411771M&cid=159227228&page=1",
-      },
-      iframeHeight: "1rem",
+      title: "",
+      contentHTML: "",
+      videoSrc: "",
+      iframeHeight: "2rem",
+      routePath: "",
     };
   },
+  methods: {
+    getVideoDefault() {
+      this.$axios
+        .get("/videos", {
+          params: {
+            path: this.routePath,
+          },
+        })
+        .then((res) => {
+          if (res.data.code == 0) {
+            const result = res.data.result;
+            this.contentHTML = result.contentHTML;
+            this.title = result.title;
+            this.videoSrc = result.video.src;
+          }
+        });
+    },
+  },
   created: function () {
-    // this.$refs.vidoeview;
-    console.log(this.$route.params.id);
+    this.routePath = this.$route.params.path;
+    this.getVideoDefault();
   },
   mounted: function () {
     this.iframeHeight = (this.$refs.videoview.clientWidth / 16) * 9 + "px";
@@ -50,10 +65,13 @@ export default {
 .video-default {
   margin-top: 2rem;
   margin-bottom: 2rem;
-  iframe {
+  .videoview {
     width: 100%;
-    // width: 16rem * 4;
-    min-height: 2rem;
+    margin-bottom: 1rem;
+    iframe {
+      width: 100%;
+      min-height: 2rem;
+    }
   }
   h3 {
     margin-top: 1rem;
