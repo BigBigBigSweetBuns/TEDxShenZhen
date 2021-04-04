33c690491d92e8888e955dd996f17b34ec3f3304
BigBigBigSweetBuns <dadadadoushabao@gmail.com>
2021-04-04 13:02:33 +0800

    add home carousel

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <b-container>
+      <home-carousel></home-carousel>
       <article-min />
       <grid-divider></grid-divider>
       <listing-section></listing-section>
@@ -13,12 +14,14 @@
 import ArticleMin from "@/components/Article/ArticleMin.vue";
 import ListingSection from "@/components/ListingSection/ListingSection.vue";
 import GridDivider from "../../components/GridDivider/GridDivider.vue";
+import HomeCarousel from './HomeCarousel.vue';
 export default {
   name: "Home",
   components: {
     ArticleMin,
     ListingSection,
     GridDivider,
+    HomeCarousel,
   },
   methods: {
     // onSlideStart(slide) {
@@ -30,3 +33,9 @@ export default {
   },
 };
 </script>
+<style lang="scss" scoped>
+.home{
+  margin-top: 1rem;
+  margin-bottom: 2rem;
+}
+</style>
---
+++ src/views/Home/HomeCarousel.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="4000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #333"
+    >
+      <!-- Slides with image only -->
+      <b-carousel-slide
+        v-for="(img, index) in banner"
+        :key="index"
+        :img-src="img.src"
+      ></b-carousel-slide>
+    </b-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "carousel",
+  props: {
+    banner: {
+      type: Object,
+      default: function () {
+        return [
+          {
+            src: "https://picsum.photos/1024/480/?image=54",
+            alt: "banner",
+          },
+          {
+            src: "https://picsum.photos/1024/480/?image=58",
+            alt: "banner",
+          },
+          {
+            src: "https://picsum.photos/1024/480/?image=55",
+            alt: "banner",
+          },
+        ];
+      },
+    },
+  },
+  data() {
+    return {
+      slide: 0,
+      sliding: null,
+    };
+  },
+  methods: {},
+};
+</script>
